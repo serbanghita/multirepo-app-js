ff9181fe3619345d6b5da9fc4a371b97b13d2dc8
Serban Ghita <sghita@adobe.com>
2017-11-27 11:03:05 +0200

    POC for including explicit classes

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,21 @@
 "use strict";
 
+var _babelRegister = require("babel-register");
+
+var _babelRegister2 = _interopRequireDefault(_babelRegister);
+
 var _Actor = require("multirepo-core/src/entity/Actor");
 
 var _Actor2 = _interopRequireDefault(_Actor);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// import * as core from "multirepo-core";
+// const Actor = core.entity.Actor;
 const a = new _Actor2.default("sghita", 10, 20, "Serban Ghita");
-console.log(a.getName());
+// babelRegister({
+//     ignore: /node_modules\/(?!multirepo-core)/,
+//     extensions: [".js"]
+// });
+
+console.log(a.getName(), a.spawn());
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import babelRegister from "babel-register";
+// babelRegister({
+//     ignore: /node_modules\/(?!multirepo-core)/,
+//     extensions: [".js"]
+// });
 import Actor from "multirepo-core/src/entity/Actor";
-
+// import * as core from "multirepo-core";
+// const Actor = core.entity.Actor;
 const a = new Actor("sghita", 10, 20, "Serban Ghita");
-console.log(a.getName());
+console.log(a.getName(), a.spawn());
